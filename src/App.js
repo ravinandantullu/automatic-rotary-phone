614f5024f4ccc3d9b3936136207e09216c6a9887
ravinandantullu <ravinandantulluri@gmail.com>
2020-04-20 10:06:07 -0500

    Added the count and line chart.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './app.module.css';
+import { Cards, Charts, CountryPicker } from './components'
+import { fetchData } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    data: {}
+  }
+
+  async componentDidMount() {
+    const { confirmed, recovered, deaths, lastUpdate } = await fetchData();
+    this.setState({data: { confirmed, recovered, deaths, lastUpdate }});
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data}/>
+        <Charts />
+        <CountryPicker />
+      </div>
+    );
+  }
 }
 
 export default App;
